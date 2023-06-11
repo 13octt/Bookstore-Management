e3d387922b2cff65ffb6deb1dfbe72989d6358e8
13octt <88072761+13octt@users.noreply.github.com>
2023-06-11 13:39:42 +0700

    Book Lookup Function

--- Bookstore_Management/Usercontrol/TraCuuSach.cs
+++ Bookstore_Management/Usercontrol/TraCuuSach.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,18 @@ namespace Bookstore_Management
 {
     public partial class TraCuuSach : UserControl
     {
+        private string connectionString;
+        private SqlConnection connection;
+        private SqlDataAdapter adapter;
+        private DataTable dataTable;
+
         public TraCuuSach()
         {
             InitializeComponent();
+            connectionString= "Data Source=DESKTOP-EQ1GSOP\\SQLEXPRESS;Initial Catalog=QLNS;Integrated Security=True;";
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+            dataTable = new DataTable();
         }
 
         private void textBox_TenSach_TextChanged(object sender, EventArgs e)
@@ -31,9 +41,40 @@ namespace Bookstore_Management
         {
             //
             String bookTitle = textBox_TenSach.Text;
-            String bookGenre = textBox_TheLoai.Text;
+            String bookCategory = textBox_TheLoai.Text;
+            string query = "SELECT * FROM THONGTINSACH WHERE TenSach LIKE @BookName AND TheLoai LIKE @Category";
 
+            // Tạo đối tượng SqlCommand
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@BookName", "%" + bookTitle + "%");
+            command.Parameters.AddWithValue("@Category", "%" + bookCategory + "%");
 
+            // Gán SqlCommand cho SqlDataAdapter
+            adapter.SelectCommand = command;
+
+            try
+            {
+                // Mở kết nối
+                connection.Open();
+
+                // Đổ dữ liệu từ database vào DataTable
+                dataTable.Clear(); // Xóa dữ liệu cũ trước khi đổ mới
+                adapter.Fill(dataTable);
+
+                // Hiển thị kết quả tra cứu trên GridView
+                dataGridView_NhapSach.DataSource = dataTable;
+                //dataGridViewBooks.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                // Xử lý exception (nếu cần)
+                MessageBox.Show("Lỗi truy vấn: " + ex.Message);
+            }
+            finally
+            {
+                // Đóng kết nối
+                connection.Close();
+            }
 
         }
 
