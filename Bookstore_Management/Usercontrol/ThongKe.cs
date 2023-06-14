b00d49f0889b5b4fdd5851c24fd35986e31b07a9
13octt <88072761+13octt@users.noreply.github.com>
2023-06-14 19:23:58 +0700

    Update ThongKe

--- Bookstore_Management/Usercontrol/ThongKe.cs
+++ Bookstore_Management/Usercontrol/ThongKe.cs
@@ -21,15 +21,12 @@ namespace Bookstore_Management
             InitializeComponent();
 
             databaseConnection = new DatabaseConnection();
-
-            //connectionString = "Data Source=DESKTOP-EQ1GSOP\\SQLEXPRESS;Initial Catalog=QLNS;Integrated Security=True;";
             connection = databaseConnection.OpenConnection();
+
             int tongKH = CountKhachHang();
             label_KhachHang.Text = tongKH.ToString();
-
             int tongSachNhap = CountSachNhap();
             label_TongSachNhap.Text = tongSachNhap.ToString();
-
             int tongSachBan = CountSachBan();
             label_TongSachBan.Text = tongSachBan.ToString();
 
@@ -37,35 +34,32 @@ namespace Bookstore_Management
 
         public int CountKhachHang()
         {
-
-                string query = "SELECT COUNT(DISTINCT MaKH) AS TongSoKhachHang FROM KHACHHANG";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    int totalCount = (int)command.ExecuteScalar();
-                    return totalCount;
-                }
+            string query = "SELECT COUNT(DISTINCT MaKH) AS TongSoKhachHang FROM KHACHHANG";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                int totalCount = (int)command.ExecuteScalar();
+                return totalCount;
+            }
         }
 
         public int CountSachNhap()
         {
-                string query = "SELECT SUM(SachDaNhap) FROM BAOCAOTONKHO";
-                using(SqlCommand command = new SqlCommand(query, connection))
-                {
-                    int totalCount = (int)command.ExecuteScalar();
-                    return totalCount;
-                }
+            string query = "SELECT SUM(SachDaNhap) FROM BAOCAOTONKHO";
+            using(SqlCommand command = new SqlCommand(query, connection))
+            {
+                int totalCount = (int)command.ExecuteScalar();
+                return totalCount;
+            }
         }
 
         public int CountSachBan()
         {
-                string query = "SELECT SUM(SachDaBan) FROM BAOCAOTONKHO";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    int totalCount = (int)command.ExecuteScalar();
-                    return totalCount;
-                }
+            string query = "SELECT SUM(SachDaBan) FROM BAOCAOTONKHO";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                int totalCount = (int)command.ExecuteScalar();
+                return totalCount;
+            }
         }
-
-
     }
 }
